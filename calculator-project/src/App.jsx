2ae773f46bb436c678e5ac0eea30babaf1b04210
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-06-16 10:37:51 +0300

    corrected css

--- calculator-project/src/App.jsx
+++ calculator-project/src/App.jsx
@@ -7,25 +7,25 @@ function App() {
         <div className="previous-operand">12 * </div>
         <div className="current-operand">34</div>
      </div>
-      <button>AC</button>
-      <button>+/-</button>
-      <button>%</button>
-      <button>÷</button>
-      <button>9</button>
+      <button className="dark-grey-btn">AC</button>
+      <button className="dark-grey-btn">+/-</button>
+      <button className="dark-grey-btn">%</button>
+      <button className="orange-btn">÷</button>
+      <button >9</button>
       <button>8</button>
       <button>7</button>
-      <button>*</button>
+      <button className="orange-btn">*</button>
       <button>6</button>
       <button>5</button>
       <button>4</button>
-      <button>-</button>
+      <button className="orange-btn">-</button>
       <button>3</button>
       <button>2</button>
       <button>1</button>
-      <button>+</button>
-      <button className="span-two">0</button>
+      <button className="orange-btn">+</button>
+      <button className="span-two rounded-bl">0</button>
       <button>,</button>
-      <button>=</button>
+      <button className="orange-btn rounded-br">=</button>
    </div>
   );
 }
