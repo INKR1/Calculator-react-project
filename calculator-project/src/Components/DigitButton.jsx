2a075fe2463c8c924d962071203a53aa882123d4
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-06-16 15:48:36 +0300

    created component DigitButton

---
+++ calculator-project/src/Components/DigitButton.jsx
@@ -0,0 +1,11 @@
+import {ADD_DIGIT} from "../constants/mathActions";
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
